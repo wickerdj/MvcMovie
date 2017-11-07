ffb99492cd25d0a9990ec47f88ea4f8aa37d28a1
GDCII\dwickersheim <dwickersheim@gdcii.com>
2017-11-07 07:00:13 -0600

    Completed 'Adding a New Field

--- Migrations/20171107125553_'AddingRating'.Designer.cs
+++ Migrations/20171107125553_'AddingRating'.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20171107125553_'AddingRating'")]
+    partial class AddingRating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
